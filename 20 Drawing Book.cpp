1f58c6de44b1e6b652809910f112622d26264ac2
dalalvarun <70593335+dalalvarun@users.noreply.github.com>
2020-11-28 08:10:59 +0530

    Level: Easy

---
+++ 20 Drawing Book.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int pageCount(int n, int p) {
+    if(n%2==0)
+        n++;
+     return min((int)p/2,(int)(n-p)/2);
+
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    int n;
+    cin >> n;
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+    int p;
+    cin >> p;
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+    int result = pageCount(n, p);
+
+    fout << result << "\n";
+
+    fout.close();
+
+    return 0;
+}
